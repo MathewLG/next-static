102439adf51e55c3a29a8ea059ad05e0aabf5f21
mathewlg <mathewlopezg@gmail.com>
2022-09-26 21:08:46 -0500

    First commit

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,8 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
+        <Header title="Bienvenidos!" />
+        <p className="Esta es la página estática del equipo.">
         </p>
       </main>
 
