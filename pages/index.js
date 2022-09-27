b4f73e248a61b9a5c19bf327c82752f2a0bef9e3
mathewlg <mathewlopezg@gmail.com>
2022-09-26 21:15:30 -0500

    Change title

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Laboratorio de aplicaciones web!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
